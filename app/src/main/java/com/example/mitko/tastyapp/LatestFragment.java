dfbb20787fdee75b7d34f7405b8f2e3e3fb0d948
dtvalkov <d.volkoff@abv.bg>
2018-05-16 12:36:17 +0300

    week3

---
+++ app/src/main/java/com/example/mitko/tastyapp/LatestFragment.java
@@ -0,0 +1,37 @@
+package com.example.mitko.tastyapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LatestFragment extends Fragment {
+    public LatestFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment LatestFragment.
+     */
+    public static LatestFragment newInstance() {
+        return new LatestFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_latest, container, false);
+    }
+}
